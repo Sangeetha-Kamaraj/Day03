1e4d79906748666a69a9a006cf38e8962221150d
Sangeetha-Kamaraj <k-sangeetha@outlook.com>
2024-01-26 21:46:47 +0530

    uploading Day03 task

---
+++ script.js
@@ -0,0 +1,27 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all")
+request.send()
+request.onload=function(){
+    var res=JSON.parse(request.response);
+    console.log(res);
+    for(i=0;i<res.length;i++){
+        console.log(res[i].name.common)
+    }
+    for(i=0;i<res.length;i++){
+
+        if (res[i].capital)
+        {
+        console.log(res[i].capital[0])
+        console.log(res[i].area)
+    }
+    else{console.log("Invalid Capital")}
+}
+
+//Qus 2: Display all the countries flag
+//Qus 3: Display all the country name,region,subregion,population
+
+for(i=0;i<res.length;i++){
+
+    console.log(res[i].flag+ " "+res[i].name.common+" "+res[i].population+" "+res[i].region+" "+res[i].subregion)
+}
+}
---
+++ script1.js
@@ -0,0 +1,22 @@
+//comparing 2 objects in JSON the same properties without order
+
+var obj1 = {"name":"Sam","class":"MCA"};
+var obj2 = {"class":"MCA","name":"Sam"};
+
+var flag=true;
+
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(var key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
+}
+else {
+    flag=false;
+}
+console.log("is object equal"+ flag);
